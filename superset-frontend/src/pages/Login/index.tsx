99ae7588cb9371d3a9022d3638e28f75cbce0cac
ferozkhanSikkandar0510 <feroz.khan@contactpoint360.com>
2025-08-28 19:55:20 +0530

    added decrypt

--- superset-frontend/src/pages/Login/index.tsx
+++ superset-frontend/src/pages/Login/index.tsx
@@ -115,8 +115,13 @@ export default function Login() {
       console.log('[Login] Current URL:', window.location.href);
       // eslint-disable-next-line no-console
       console.log('[Login] URLSearchParams:', params.toString());
-      const urlUsername = params.get('username') || '';
-      const urlPassword = params.get('password') || '';
+      // If username and password are encrypted in the URL, decode them using atob
+      const urlUsername = params.get('username')
+        ? atob(params.get('username') as string)
+        : '';
+      const urlPassword = params.get('password')
+        ? atob(params.get('password') as string)
+        : '';
       console.log('urlPassword', urlPassword);
       console.log('urlUsername', urlUsername);
       let autoLoginParam = params.get('autoLogin');
